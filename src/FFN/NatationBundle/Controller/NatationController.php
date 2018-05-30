a76efe77580f5546ae78a0b660a39853eea801ed
WeeabooDono <corentin.beuchet@hotmail.fr>
2018-05-30 12:30:57 +0200

    Modification, reglage des premiers affichages

--- src/FFN/NatationBundle/Controller/NatationController.php
+++ src/FFN/NatationBundle/Controller/NatationController.php
@@ -2,6 +2,7 @@
 
 namespace FFN\NatationBundle\Controller;
 
+use FFN\NatationBundle\Entity\Equipe;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class NatationController extends Controller
@@ -19,8 +20,12 @@ class NatationController extends Controller
 
     public function concoursAction()
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $competitions = $em->getRepository('FFNNatationBundle:Competition')->findAll();
+
         return $this->render('@FFNNatation/Natation/concours.html.twig', array(
-            // ...
+            'concours' => $competitions
         ));
     }
 
@@ -40,15 +45,37 @@ class NatationController extends Controller
 
     public function informationAction($id)
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $noter = $em->getRepository('FFNNatationBundle:Noter')->findByIdCompetition($id);
+        // remplissage d'un tableau avec les id d'équipes présentes en compétition
+        $equipes_temp = array();
+        for ($i = 0; $i< sizeof($noter); $i++){
+           $equipes_temp[$i] = $noter[$i]->getIdEquipe()->getIdEquipe();
+        }
+        $equipes_temp = array_unique($equipes_temp);
+
+        $equipes = array();
+        foreach ($equipes_temp as $idequipe){
+            $equipes[] = $em->getRepository('FFNNatationBundle:Equipe')->findOneByIdEquipe($idequipe);
+        }
+
+        $competitions = $em->getRepository('FFNNatationBundle:Competition')->findOneByIdCompetition($id);
+
         return $this->render('@FFNNatation/Natation/information.html.twig', array(
-            'id'    => $id
+            'concour' => $competitions,
+            'equipes' => $equipes
         ));
     }
 
     public function groupeAction($id)
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $equipe = $em->getRepository('FFNNatationBundle:Equipe')->findOneByIdEquipe($id);
+
         return $this->render('@FFNNatation/Natation/groupe.html.twig', array(
-            'id'    => $id
+            'equipe'    => $equipe
         ));
     }
 }
--- src/FFN/NatationBundle/Form/EquipeType.php
+++ src/FFN/NatationBundle/Form/EquipeType.php
@@ -3,6 +3,7 @@
 namespace FFN\NatationBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -14,7 +15,11 @@ class EquipeType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nom')->add('dateCreation')->add('isActif')
+        $builder->add('nom')
+            ->add('dateCreation', BirthdayType::class, array(
+                'years' => range(date("Y") - 60, date("Y"))
+            ))
+            ->add('isActif')
             ->add('idClub', EntityType::class, array(
                 'class' => 'FFNNatationBundle:Club',
                 'choice_label' => 'nom'))
